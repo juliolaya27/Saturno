5ae35adcb1821d49630d1633a23ea76cc23aa84c
juliolaya27 <juliolaya27@gmail.com>
2023-01-19 18:00:16 -0400

    cambios en la rama de prueba
    cambios en la rama de prueba

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine("Este es el ciclo número: " + (i + 1));
-}
 
+
+
+    for (int j = 0; j < 19; j++)
+    {
+        Console.WriteLine("Este es el ciclo número: " + (j + 1));
+    }
+
+Console.WriteLine("Bye, World!");
 
